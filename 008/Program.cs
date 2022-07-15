35b207e4b858f26dadec96493fc204d7dda43468
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-15 21:29:51 +0500

    008

---
+++ 008/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstNumber = Convert.ToInt32(System.Console.WriteLine());
+int secondNumber = Convert.ToInt32(System.Console.WriteLine());
+if(firstNumber % secondNumber == 0)
+{
+    System.Console.WriteLine("КРАТНО");
+}
+else
+{
+    System.Console.WriteLine($"ОСТАТОК: {firstNumber % secondNumber}");
+}
