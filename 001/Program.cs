fceb95739acce6d55bc3021dc3a116041199d863
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-15 20:43:37 +0500

    001

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿int namber = new Random().Next(10,100);
+System.Console.WriteLine(namber);
+int firstDigit = namber / 10;
+int secondDigit = namber % 10;
+System.Console.WriteLine(firstDigit);
+System.Console.WriteLine(secondDigit);
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Первое число {firstDigit} больше");
+}
+else if
+{
+    Console.WriteLine($"Второе число {secondDigit} больше");
+}
+else
+{
+    Console.WriteLine($"Равны");
+}
+
