51bf034286528552df474835a5d9033e719d1c0c
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-15 21:55:46 +0500

    010

---
+++ 010/Program.cs
@@ -0,0 +1,6 @@
+﻿int inputValue = Convert.ToInt32(Console.WriteLine());
+bool IsMultiplicity(int number);
+{
+    return(number % 7 == 0 && number % 23 == 0);
+}
+Console.WriteLine(IsMultiplicity(inputValue));
