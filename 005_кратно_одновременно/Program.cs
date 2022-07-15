f1abbb5def3b252fd1239e7297ce90a0b698e8b8
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-15 20:46:41 +0500

    005

---
+++ 005_кратно_одновременно/Program.cs
@@ -0,0 +1,6 @@
+﻿int inputValue = Convert.ToInt32(Console.WriteLine());
+bool IsMultiplicity (int namber)
+{
+    return(namber %7 == 0 && namber % 23 == 0);
+}
+Console.WriteLine(IsMultiplicity(inputValue));
