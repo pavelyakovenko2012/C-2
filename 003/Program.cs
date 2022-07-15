704ada7ab7b3ffb9ff5453c02d1c36fd01547d7d
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-15 20:45:07 +0500

    003

---
+++ 003/Program.cs
@@ -0,0 +1,11 @@
+﻿string namber = Console.ReadLine();
+if(namber.Length < namber[2])
+{
+    Console.Write("Введите трехзначное или большее число");
+}
+else
+{
+    Console.Write(namber[2]);
+}
+
+
